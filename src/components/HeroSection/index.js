af8ce71c81589f4f1961a33447c26785437c20e9
Chandu_Kommineni <chandukommineni63@gmail.com>
2024-05-16 21:34:33 +0530

    version 1

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,71 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle, ResumeButton } from './HeroStyle'
+import HeroImg from '../../images/HeroImage.jpg'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+import {Tilt } from "react-tilt";
+import {motion} from "framer-motion";
+import {headTextAnimation,headContainerAnimation,headContentAnimation} from "../../utils/motion"
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+
+               <motion.div {...headContainerAnimation}>
+               
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        <motion.div {...headTextAnimation}>
+                        <Title>Hi, I am <br /> {Bio.name}</Title>
+                        <TextLoop>
+                            I am a
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.roles,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>                        
+                            </TextLoop>
+                            <SubTitle>{Bio.description}</SubTitle>
+                        </motion.div>
+                                               
+        
+                        <ResumeButton href={Bio.resume} target='display'>View Resume</ResumeButton>
+                        
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+                        <motion.div {...headContentAnimation}>
+                        <Tilt
+                            options={{ max: 25, scale: 1.05 }}
+                            style={{
+                                display: 'flex',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                perspective: '1000px',
+                            }}
+                        >
+                            <Img src={HeroImg} alt="hero-image" />
+                        </Tilt>
+
+                        </motion.div>
+                       
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+                
+               </motion.div>
+                
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
---
+++ src/components/Projects/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import { useState } from 'react'
+import { Container, Wrapper, Title, Desc, CardContainer, ToggleButtonGroup, ToggleButton, Divider } from './ProjectsStyle'
+import ProjectCard from '../Cards/ProjectCards'
+import { projects } from '../../data/constants'
+
+
+const Projects = ({openModal,setOpenModal}) => {
+  const [toggle, setToggle] = useState('all');
+  return (
+    <Container id="projects" style={{marginTop:"20px"}}>
+      <Wrapper>
+        <Title>Projects</Title>
+        <Desc>
+        With a strong foundation in web application development, I'm passionate about creating user-friendly and impactful web experiences.
+        </Desc>
+        <ToggleButtonGroup >
+          {toggle === 'all' ?
+            <ToggleButton active value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+            :
+            <ToggleButton value="all" onClick={() => setToggle('all')}>All</ToggleButton>
+          }
+          <Divider />
+          {toggle === 'web app' ?
+            <ToggleButton active value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+            :
+            <ToggleButton value="web app" onClick={() => setToggle('web app')}>WEB APP'S</ToggleButton>
+          }
+          <Divider />
+        
+        </ToggleButtonGroup>
+        <CardContainer>
+          {toggle === 'all' && projects
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+          {projects
+            .filter((item) => item.category === toggle)
+            .map((project) => (
+              <ProjectCard project={project} openModal={openModal} setOpenModal={setOpenModal}/>
+            ))}
+        </CardContainer>
+      </Wrapper>
+    </Container>
+  )
+}
+
+export default Projects
